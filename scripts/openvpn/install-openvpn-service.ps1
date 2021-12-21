a8f6751d3b5a837f08f6b166003632d451585bfd
Cristi Sabaila <3983569+SabaSCP@users.noreply.github.com>
2021-12-21 18:59:15 +0200

    Update install-openvpn-service.ps1
    configure restart policy

--- scripts/openvpn/install-openvpn-service.ps1
+++ scripts/openvpn/install-openvpn-service.ps1
@@ -12,3 +12,6 @@ New-Service -BinaryPathName "$app_root\bin\openvpnserv2.exe" `
     -StartupType Manual
 
 Start-Service -Name "OpenVPNServiceInteractive"
+
+sc.exe failure OpenVPNServiceInteractive reset= 30 actions= restart/60000
+sc.exe failure OpenVPNService reset= 30 actions= restart/60000
